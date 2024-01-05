9df03dc26c29d52bbac8dc85ce52559424d61a00
wolfpac <cbellec@normandiewebschool.fr>
2024-01-05 12:29:32 +0100

    update kpi + add page

--- methodes/kpi.php
+++ methodes/kpi.php
@@ -51,41 +51,12 @@ function generateKPICard($title, $value)
             echo '</div>';
 }
 
+// Récupère le nombre de visiteurs quotidiens depuis la session
+$nombreVisiteursQuotidiens = isset($_SESSION['nombreVisiteursQuotidiens']) ? $_SESSION['nombreVisiteursQuotidiens'] : 0;
 
 
 
-function generateVisitorsCard($pdo)
-{
-    // Récupère la date actuelle
-    $dateActuelle = date("Y-m-d");
 
-    // Vérifie si une entrée pour la date actuelle existe déjà dans la base de données
-    $stmt = $pdo->prepare("SELECT * FROM visitorcount WHERE date = :date");
-    $stmt->bindParam(':date', $dateActuelle);
-    $stmt->execute();
-
-    if ($stmt->rowCount() > 0) {
-        // Met à jour le compteur s'il y a déjà une entrée pour la date actuelle
-        $pdo->query("UPDATE visitorcount SET visitor_num = visitor_num + 1 WHERE date = '$dateActuelle'");
-    } else {
-        // Insère une nouvelle entrée pour la date actuelle si elle n'existe pas encore
-        $stmt = $pdo->prepare("INSERT INTO visitorcount (date, visitor_num) VALUES (:date, 1)");
-        $stmt->bindParam(':date', $dateActuelle);
-        $stmt->execute();
-    }
-
-    // Récupère le nombre de visiteurs pour la date actuelle
-    $stmt = $pdo->prepare("SELECT visitor_num FROM visitorcount WHERE date = :date");
-    $stmt->bindParam(':date', $dateActuelle);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $nombreVisiteurs = $row['visitor_num'];
-
-    // Affiche le nombre de visiteurs
-    echo '<div class="mb-6">';
-    echo '<p class="text-lg">Nombre de visiteurs aujourd\'hui : <span class="font-bold">' . $nombreVisiteurs . '</span></p>';
-    echo '</div>';
-}
 // Ajoutez d'autres KPI ici
 
 ?>
